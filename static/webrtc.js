e5d58a1757cca04dbcf3fd68ee76bb98ac6052d0
Maksim Kozhukh <mkozhukh@ya.ru>
2017-07-18 23:49:03 +0300

    [dev] final code reorganization

--- static/webrtc.js
+++ static/webrtc.js
@@ -1,3 +1,58 @@
+var config = {
+	server:"localhost:8080",
+	users:function(room, people, me){
+		var list = $$("contactsList");
+		list.clearAll();
+
+		if (config.name)
+			list.add({ 
+				id:-1, 
+				img:"images/avatar.jpg", 
+				title: config.name + " ( this is Me )" });
+
+		for (var key in people){
+			var v = people[key];
+			list.add ({
+				id: v.easyrtcid,
+				img: "images/avatar.jpg",
+				title: v.username
+			});
+		}
+	}
+};
+
+var contactsList = {
+	header : "Rooms",
+	view : "list",
+	id : "contactsList",
+	template : `
+		<div class='contactPaneDiv'>
+			<img class="contactIcon" src="#img#"/>
+			<span class="contactTextDiv">#title#</span>
+		</div>
+	`,
+	item : {
+		height: 80,
+		width: 300,
+	},
+	select:true, scroll:false,
+	on:{ onBeforeSelect: (id) => doCall(id) }
+};
+
+var chat = {
+	css:"absarea",
+	template:`<div class='mirrorDiv'><video id='mirrorVideo' width></div>
+				<div class='windowToUniverseDiv'><video id='windowToUniverse'></div>`
+};
+
+
+function funnyName(){
+	var first = ["Agile", "Strong", "Tricky", "Shiny", "Gloom"];
+	var second = ["Tree", "Cat", "Boss", "User", "Rabbit"];
+
+	return first[Math.floor(Math.random()*first.length)] + " " + second[Math.floor(Math.random()*second.length)];
+}
+
 function doConnect(config) {
 	easyrtc.setVideoDims(640,480);
 	easyrtc.setUsername (config.name);
@@ -59,3 +114,50 @@ function doCall(easyrtcid) {
 		}
 	);
 }
+
+
+webix.ready(function(){
+
+	webix.ui({
+		rows : [
+			{ view:"toolbar", cols:[
+				{ view:"label", label : "Webix WebRTC Chat" },
+				{},
+				{ view:"label", id:"status", css:"status", value:"", width: 200 },
+				{ view:"button", id:"endcall", value:"End Call", width: 100, click:function(){
+					$$("endcall").hide();
+					easyrtc.hangupAll();
+					$$("contactsList").unselectAll()
+					$$("status").setValue("");
+				}, hidden:true }
+			]},
+			{ cols :[
+				contactsList,
+				chat
+			]}
+		]
+	});
+
+	var win = webix.ui ({
+		view: "window", position:"top", head:false, modal:true,
+		body: {
+			view:"form", rows:[
+				{ view:"text", name:"name", label:"You Name", value:funnyName() },
+				{ view:"button", value:"Start !", click:function(){
+					var name = this.getFormView().getValues().name;
+					if (!easyrtc.isNameValid (name))
+						webix.message({ type:"error", text:"Invalid name" });
+					else {
+						this.getTopParentView().hide();
+						config.name = name;
+						doConnect(config);
+					}
+				}}
+			]
+		}
+	});
+
+	win.show();
+	var input = win.getBody().elements.name.getInputNode();
+	input.select(); input.focus();
+});
